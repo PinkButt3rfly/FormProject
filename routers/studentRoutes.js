22214342c99f4da3dd7697416a47b1d0e992bd62
nahloo-obi <114304088+nahloo-obi@users.noreply.github.com>
2024-04-29 22:54:30 +0100

    Update studentRoutes.js

--- routers/studentRoutes.js
+++ routers/studentRoutes.js
@@ -1,18 +1,18 @@
 const express = require('express')
-const {registerStudent} = require('../controllers/studentController')
+const studentcontroller = require('../controllers/studentController')
 
 
 const router = express.Router()
 
-//router.post('register', registerStudent)
+router.post('register', studentcontroller.registerStudent)
 
-router.post('/register', (req, res) => {
-    const formData = req.body;
+// router.post('/register', (req, res) => {
+//     const formData = req.body;
 
-    console.log('Recieved form data:', formData);
+//     console.log('Recieved form data:', formData);
 
-    res.json({ message: 'Registration successful'})
-});
+//     res.json({ message: 'Registration successful'})
+// });
 
 
 module.exports = router;
